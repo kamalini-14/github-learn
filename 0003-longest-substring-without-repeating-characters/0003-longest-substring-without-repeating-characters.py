cb05150ac064b09760f39603be527bbb19f1298e
Kamalini V <kamaliniv24am@srishakthi.ac.in>
2026-06-27 22:57:31 +0530

    Time: 55 ms (17.05%), Space: 13 MB (66.5%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -8,12 +8,9 @@ class Solution(object):
         l=0
         c=0
         for r in range(len(s)):
-            if s[r] in a:
-                i=0
-                while(s[r] in a):
-                    a=a[1:]
-                    i+=1
-                    l+=1
+            while(s[r] in a):
+                a=a[1:]
+                l+=1
             c=max(c,r-l+1)
             a.append(s[r])
         return c
